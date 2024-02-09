c78f9f8aa3f6981a1e4a59c360b74adb7b86c708
MARIA GABRIELLY GINO MAIERO <maria.ggmaiero@senacsp.edu.br>
2024-02-08 22:01:40 -0300

    atividade

---
+++ atividade5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Ler dois valores (considere que não serão lidos valores iguais e escreve-los em ordem crescente
+Console.WriteLine("Ordem Crescente");
+
+Console.WriteLine("Digite o primeiro número:");
+int numero1 = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Digite o primeiro número:");
+int numero2 = Convert.ToInt16(Console.ReadLine());
+
+if (numero1 > numero2)
+    Console.WriteLine("O maior" + numero1 + "E o menor" + numero2);
+
+if (numero2 > numero1)
+    Console.WriteLine("O maior" + numero2 + "E o menor" + numero1);
+
+
